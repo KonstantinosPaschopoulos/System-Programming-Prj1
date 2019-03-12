82e20488fe14037c4c8b88141615dfc0827a7517
KonstantinosPaschopoulos <sdi1500127@di.uoa.gr>
2019-03-12 19:52:42 +0200

    removed / from commands

--- command_functions.c
+++ command_functions.c
@@ -18,46 +18,46 @@ int get_command(char *input){
   pch = strtok(copy_input, " ");
   if (pch != NULL)
   {
-    if (strcmp(pch, "/requestTransaction") == 0)
+    if (strcmp(pch, "requestTransaction") == 0)
     {
       return 1;
     }
-    else if (strcmp(pch, "/requestTransactions") == 0)
+    else if (strcmp(pch, "requestTransactions") == 0)
     {
       //Checking to see there is a semicolon in the rest of command
       semicolon_position = strchr(semicolon_copy, ';');
       if (semicolon_position == NULL)
       {
-        //This is the second case of the /requestTransactions command
+        //This is the second case of the requestTransactions command
         return 3;
       }
       else
       {
-        //This is the first case of the /requestTransactions command
+        //This is the first case of the requestTransactions command
         return 2;
       }
     }
-    else if (strcmp(pch, "/findEarnings") == 0)
+    else if (strcmp(pch, "findEarnings") == 0)
     {
       return 4;
     }
-    else if (strcmp(pch, "/findPayments") == 0)
+    else if (strcmp(pch, "findPayments") == 0)
     {
       return 5;
     }
-    else if (strcmp(pch, "/walletStatus") == 0)
+    else if (strcmp(pch, "walletStatus") == 0)
     {
       return 6;
     }
-    else if (strcmp(pch, "/bitCoinStatus") == 0)
+    else if (strcmp(pch, "bitCoinStatus") == 0)
     {
       return 7;
     }
-    else if (strcmp(pch, "/traceCoin") == 0)
+    else if (strcmp(pch, "traceCoin") == 0)
     {
       return 8;
     }
-    else if (strcmp(pch, "/exit") == 0)
+    else if (strcmp(pch, "exit") == 0)
     {
       return 9;
     }
@@ -160,8 +160,8 @@ void requestTransactions(char *user_input, wallet *walletList, table *senderHash
   //Using fgets to get the rest of the user's commands
   while (fgets(whole_line, MAX_INPUT, stdin))
   {
-    //The user must type /exit to exit this command
-    if (strcmp(whole_line, "/exit\n") == 0)
+    //The user must type exit to exit this command
+    if (strcmp(whole_line, "exit\n") == 0)
     {
       printf("Exiting requestTransactions.\n");
       return;
